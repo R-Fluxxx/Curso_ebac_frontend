a2d09cc15171e92761ce6ba8c204753e8714cab0
Jefferson Souza <110081786+R-Fluxxx@users.noreply.github.com>
2024-02-08 13:53:29 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="calculator">
+    <input type="number" v-model.number="numero1" @input="calcular">
+    <select v-model="operacao" @change="calcular">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input type="number" v-model.number="numero2" @input="calcular">
+    <div>Resultado: {{ resultado }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: '+',
+      resultado: 0
+    };
+  },
+  methods: {
+    calcular() {
+      switch (this.operacao) {
+        case '+':
+          this.resultado = this.numero1 + this.numero2;
+          break;
+        case '-':
+          this.resultado = this.numero1 - this.numero2;
+          break;
+        case '*':
+          this.resultado = this.numero1 * this.numero2;
+          break;
+        case '/':
+          this.resultado = this.numero1 / this.numero2;
+          break;
+        default:
+          this.resultado = 0;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+input, select {
+  margin: 5px;
+  padding: 5px;
+}
+</style>
