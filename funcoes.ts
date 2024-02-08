15acc2ebe8376f69960a87fb83c2d2538e2337fa
Jefferson Souza <110081786+R-Fluxxx@users.noreply.github.com>
2024-02-08 13:24:51 -0300

    Add files via upload

---
+++ funcoes.ts
@@ -0,0 +1,9 @@
+// Função de multiplicação
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
